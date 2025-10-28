ee0fd3544d5cd8460f6d6f0b61dbd3de5b8824ac
jdecorte-be <jdecorte@proton.me>
2025-10-28 16:09:09 +0000

    feat(scripts): Add script to automate router configuration application

---
+++ p3/apply-conf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+running_containers=$(docker ps -q)
+
+for container_id in $running_containers; do
+  hostname=$(docker exec "$container_id" hostname)
+
+  if [[ "$hostname" =~ ^(host_|router_).+ ]]; then
+    filename="$hostname"
+    echo $filename
+    if [[ ! -f "$filename" ]]; then
+      echo "File $filename not found. Skipping container $hostname."
+      continue
+    fi
+
+    echo "Applying configuration $filename on container $hostname ($container_id)..."
+
+    docker cp "$filename" "$container_id":/
+    docker exec "$container_id" ash "/$filename"
+
+    echo "Configuration applied on $hostname ($container_id)."
+  else
+    echo "Skipping container $hostname ($container_id), hostname does not match."
+  fi
+done
